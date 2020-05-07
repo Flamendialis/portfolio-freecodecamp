6c383abb8b3b0581b800a4f9fa9601c74aa13a32
Flamen_Dialis <menendezrfelixj@gmail.com>
2020-05-07 01:46:37 -0300

    All sections done

--- resources/script.js
+++ resources/script.js
@@ -1,7 +1,17 @@
 function solidHeader() {
   if (window.scrollY > 50) {
     document.getElementById("header").classList.add("black-header");
+    document.getElementById("navlist").classList.add("small-nav");
+    Array.prototype.forEach.call(
+      document.getElementsByClassName("nav-link"),
+      (link) => link.classList.add("small-link")
+    );
   } else {
     document.getElementById("header").classList.remove("black-header");
+    document.getElementById("navlist").classList.remove("small-nav");
+    Array.prototype.forEach.call(
+      document.getElementsByClassName("nav-link"),
+      (link) => link.classList.remove("small-link")
+    );
   }
 }
